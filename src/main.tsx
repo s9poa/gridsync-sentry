063b9cb9684ce705fe7e5240894a501d8e04921c
s9poa <s9poah@gmail.com>
2025-07-15 00:36:35 -0400

    Designed Header for Desktop

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import './frontend/css/directory-css/index.css';
 import './assets/fontawesome/css/all.min.css';
 import './frontend/css/universal.css';
 
+import Header from './frontend/component-builder/Header';
 import Home from './frontend/directory/Home';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
 )
